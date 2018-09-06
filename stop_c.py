ac58f468c5fdac1c9baf75f6918b9d59989e8ee0
simon-mo <xmo@berkeley.edu>
2018-09-06 20:29:58 +0000

    Further cleanup

--- stop_c.py
+++
@@ -1,4 +0,0 @@
-from clipper_admin import ClipperConnection, KubernetesContainerManager, DockerContainerManager
-
-clipper_conn = ClipperConnection(DockerContainerManager())
-clipper_conn.stop_all()
