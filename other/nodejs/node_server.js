699428ef635583149aa1d4da0e147678edabb2a0
Simon Mo <xmo@berkeley.edu>
2018-09-10 23:44:51 -0400

    Make node_server work on Chrome+Firefox+Safari

--- other/nodejs/node_server.js
+++ other/nodejs/node_server.js
@@ -5,7 +5,7 @@ const port = 8080
 
 const requestHandler = (request, response) => {
 	response.setHeader('Access-Control-Allow-Origin', '*');
-	response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Content-Type");
+	response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Content-Type, Origin, Content-Type, X-Requested-With, Accept, Authorization");
 	console.log(request.method)
 	if(request.method =="POST"){
 		var body = "";
@@ -17,22 +17,11 @@ const requestHandler = (request, response) => {
 		    var jsonObj = JSON.parse(body);
 		  	console.log(jsonObj);
 		  	var toWrite = [jsonObj['label'], jsonObj['leftPaddle_y'], jsonObj['ball_x'], jsonObj['ball_y'], jsonObj['ball_dx'], jsonObj['ball_dy'], jsonObj['ball_x_prev'], jsonObj['ball_y_prev']]
-		  	// toWrite += jsonObj['label']+","+jsonObj['leftPaddle_y']+","+jsonObj['ball_x'] +","+jsonObj['ball_y'] +","+jsonObj['ball_dx']+","+jsonObj['ball_dy'] +","+jsonObj['ball_x_prev']+","+jsonObj['ball_y_prev']
-		  	// write to a  file named output.txt
 
-		  	//var writer = csvWriter({ headers: ["label","paddle_y","0","ball_x","ball_y","ball_dx","ball_dy","x_prev","y_prev"]})
 			var writer = csvWriter({sendHeaders: false})
 			writer.pipe(fs.createWriteStream('out.csv', {flags: 'a'}))
 			writer.write(jsonObj)
 			writer.end()
-			// csv.from(line).to(fs.createWriteStream(path, {flags: 'a'}))
-			// fs.createWriteStream('output.csv', toWrite, (err) => {  
-			//     // throws an error, you could also catch it here
-			//     if (err) throw err;
-
-			//     // success case, the file was saved
-			//     console.log('Lyric saved!');
-			// });
 		  })		
 		response.end("Data recorded")
 	}
